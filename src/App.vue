c50263151b469e8674f70383bafaa7fd18e3904a
เรืองศักดิ์ บุษบงก์ไพฑูรย์ <reangsak.b@Macintosh-2.local>
2025-02-14 14:35:21 +0700

    feat: add hero section into landing page

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 import { defineAsyncComponent } from "vue";
 // Import Components Asynchronously
 const Navbar = defineAsyncComponent(() => import("@/components/NavBar.vue"));
+const HeroSection = defineAsyncComponent(() =>
+  import("@/components/HeroSection.vue")
+);
 </script>
 
 <template>
   <div class="bg-[#111827] min-h-screen">
     <Navbar />
+    <HeroSection />
   </div>
 </template>
