42393286c73b90158799e9ced6ed94b53e10fcae
tong <reangsak1234@hotmail.com>
2025-02-16 14:19:46 +0700

    feat: add back to top button and loading animation into landing page

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,40 @@ const ContactSection = defineAsyncComponent(() =>
   import("@/components/ContactSection.vue")
 );
 const Footer = defineAsyncComponent(() => import("@/components/Footer.vue"));
+const BackToTop = defineAsyncComponent(() =>
+  import("@/components/BackToTop.vue")
+);
+import LoadingSpinner from "./components/LoadingSpinner.vue";
 </script>
 
 <template>
   <div class="bg-[#111827] min-h-screen">
-    <Navbar />
-    <HeroSection />
-    <ServicesSection />
-    <AboutSection />
-    <ExperienceAndSkills />
-    <ProjectSection />
-    <ContactSection />
-    <Footer />
+    <Suspense>
+      <template #default>
+        <div>
+          <Navbar />
+          <HeroSection />
+          <ServicesSection />
+          <AboutSection />
+          <ExperienceAndSkills />
+          <ProjectSection />
+          <ContactSection />
+          <Footer />
+          <BackToTop />
+        </div>
+      </template>
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+          <LoadingSpinner />
+        </div>
+      </template>
+    </Suspense>
   </div>
 </template>
+
+<style>
+* {
+  scrollbar-width: thin;
+  scrollbar-color: #111827 #f1f1f1;
+}
+</style>
